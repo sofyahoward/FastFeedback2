a6df7849b9440d91ccb4aafafd68a43cee7b3fea
AHowardC <achsvgusaeng@yahoo.com>
2018-07-31 14:56:18 -0400

    set up react router

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,12 +1,20 @@
 //this file contains Redux 
 //this file is also redering the app.js (main component)
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-import App from './components/App'
+import App from './components/App';
+import reducers from './reducers'
+
+//creating a redux store for state management
+//first argument is a reducer, second is initial state usually usefull for server side rendering, which is not necessary for us. So we put an empty object, the third is our middlewear.
+const store = createStore(reducers, {}, applyMiddleware())
+
 //use JSX tags because react expects a component instance as an argument
 ReactDOM.render(
-    <App/>,
+    //provider tag is a react component that brings together the react and redux sides of the application together. It informs all of its' child components (App) that a new state is available through Redux.
+    <Provider store={store}><App/></Provider>,
     document.querySelector('#root')
 );
---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,9 @@
+//understand reducers more
+
+//auth reducer is here to decide whether the user is currently logged in
+export default function (state = {}, action){
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
