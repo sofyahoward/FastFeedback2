5617ab1e63a8ed78398fe734df8635c46f945fe7
AHowardC <achsvgusaeng@yahoo.com>
2018-08-09 18:09:13 -0400

    added client side and billing

--- server/index.js
+++ server/index.js
@@ -31,6 +31,20 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV ==='production'){
+  //express will serve up production assets
+  //like our main.js file, or main.css file 
+  //very particular file path
+  //this tells express server to look for a specific file inside its directory if we don't have a route handler for a requested route
+  app.use(express.static('client/build'))
+
+  //Express will serve up the index.html file if it doesnt recognize the route after trying to match up the route above.
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+
+}
 
 // make port constant to dynamically bind based on heroku port assignment or port 5000 on local
 const PORT = process.env.PORT||5000;
